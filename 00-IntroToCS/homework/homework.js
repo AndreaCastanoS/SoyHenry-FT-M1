41a4ea589fe1acd830841c0d8ab799eb59e97272
Andrea <andreacastsalazar@gmail.com>
2022-04-29 12:39:27 -0500

    First commit

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,10 +3,21 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  let suma = 0 ;
+  for (let i = 0; i< num.length; i++){
+  suma = suma + num [i] *2 ** (num.length-1-i);
+  }
+   return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binario = '';
+  while (num > 0){
+    binario =(num % 2) + binario;
+    num=Math.floor(num/2);
+  }
+  return binario;
 }
 
 // No se pueden usar: 
