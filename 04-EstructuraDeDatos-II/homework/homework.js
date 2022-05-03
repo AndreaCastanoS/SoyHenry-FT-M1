506752d972197017e688f4f58eb8e2aa7dfc107d
Andrea <andreacastsalazar@gmail.com>
2022-05-02 19:44:10 -0500

    Ok

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -11,12 +11,81 @@
 // search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
 
 function LinkedList() {
-
+this.head = null;
 }
 
 function Node(value){
+  this.value = value;
+  this.next = null;
+}
+ 
+let list = new LinkedList();
+
+LinkedList.prototype.add= function (value){
+  let node = new Node (value);
+
+  
+   if(!this.head){
+    this.head = node;
+  }else{
+    let current = this.head; 
+    while (current.next){
+      current = current.next;
+    }
+  current.next = node;
+  }
+} 
+
+
+LinkedList.prototype.remove = function (){
+ 
+
+
+  if (this.head == null) return null;
+if  (!this.head.next){
+  var aux1 = this.head.value;  
+     this.head = null
+
+     return aux1;
+  }
+ else {
+      let current = this.head
+      while (current.next.next){
+        current = current.next;
+      }
+        var aux = current.next.value;
+        current.next = null;
+      
+      return aux;
+    }
 
 }
+
+LinkedList.prototype.search = function (argumento){
+  
+  let current = this.head;
+  if(!current) return null;
+
+  while (current){
+     if (typeof argumento === 'function'){
+       if ( argumento(current.value)){
+       return current.value;
+       }
+     }else {
+
+    if(current.value === argumento) return argumento;
+     }
+    current = current.next; 
+     
+  }
+     return null;
+}
+
+
+
+
+
+
 
 // Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
 // Una Hash table contiene un arreglo de "contenedores" o buckets donde puede guardar información.
@@ -31,8 +100,46 @@ function Node(value){
 //    - Retornar dicho valor.
 
 function HashTable() {
+  this.numBuckets = 35,
+  this.buckets = [];
 
 }
+
+HashTable.prototype.hash = function (key){
+  var acum = 0;
+  for (var i = 0; i < key.length; i++){
+    acum = acum + key.charCodeAt (i);
+  }
+  return acum % this.numBuckets;
+}
+
+HashTable.prototype.set = function (key, value){
+  if (typeof  key !== 'string') throw new TypeError ('keys must be string');
+  var index = this.hash (key);
+  if (!this.buckets[index]){ 
+    this.buckets[index]= {}
+  }
+  this.buckets[index][key] = value;
+}
+
+HashTable.prototype.get = function (key) {
+var index = this.hash (key);
+return this.buckets[index][key];
+}
+
+HashTable.prototype.hasKey = function () {
+  var index  = this.hash (key);
+  return this.buckets[index]. hasOwnProperty (key)
+  
+}
+
+
+
+
+
+
+
+
 
 
 // No modifiquen nada debajo de esta linea
